a7c0946204a516eef9253e1194729ee73f2e3452
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-04-03 18:02:04 +0100

    Criação da view para ver os dados

---
+++ app/src/main/java/pt/ipg/enviodados/activity_LerDados.java
@@ -0,0 +1,14 @@
+package pt.ipg.enviodados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_LerDados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__ler_dados);
+    }
+}
